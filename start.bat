fbf97245f037bfc7e7e6577b80afee6b80d3ba45
yxxzs <kannna04120405@yahoo.co.jp>
2024-09-21 09:13:54 +0900

    Start.bat

---
+++ start.bat
@@ -0,0 +1,194 @@
+@echo off
+python main.py
+mode con: cols=99 lines=45
+
+echo Enter press for restart
+pause
+cls
+python main.py
+pause
+cls
+python main.py
+pause
+cls
+python main.py
+pause
+cls
+python main.py
+pause
+cls
+python main.py
+pause
+cls
+python main.py
+pause
+cls
+python main.py
+pause
+cls
+python main.py
+pause
+cls
+python main.py
+pause
+cls
+python main.py
+pause
+cls
+python main.py
+pause
+cls
+python main.py
+pause
+cls
+python main.py
+pause
+cls
+python main.py
+pause
+cls
+python main.py
+pause
+cls
+python main.py
+pause
+cls
+python main.py
+pause
+cls
+python main.py
+pause
+cls
+python main.py
+pause
+cls
+python main.py
+pause
+cls
+python main.py
+pause
+cls
+python main.py
+pause
+cls
+python main.py
+pause
+cls
+python main.py
+pause
+cls
+python main.py
+pause
+cls
+python main.py
+pause
+cls
+python main.py
+pause
+cls
+python main.py
+pause
+cls
+python main.py
+pause
+cls
+python main.py
+pause
+cls
+python main.py
+pause
+cls
+python main.py
+pause
+cls
+python main.py
+pause
+cls
+python main.py
+pause
+cls
+python main.py
+pause
+cls
+python main.py
+pause
+cls
+python main.py
+pause
+cls
+python main.py
+pause
+cls
+python main.py
+pause
+cls
+python main.py
+pause
+cls
+python main.py
+pause
+cls
+python main.py
+pause
+cls
+python main.py
+pause
+cls
+python main.py
+pause
+cls
+python main.py
+pause
+cls
+python main.py
+pause
+cls
+python main.py
+pause
+cls
+python main.py
+pause
+cls
+python main.py
+pause
+cls
+python main.py
+pause
+cls
+python main.py
+pause
+cls
+python main.py
+pause
+cls
+python main.py
+pause
+cls
+python main.py
+pause
+cls
+python main.py
+pause
+cls
+python main.py
+pause
+cls
+python main.py
+pause
+cls
+python main.py
+pause
+cls
+python main.py
+pause
+cls
+python main.py
+pause
+cls
+python main.py
+pause
+cls
+python main.py
+pause
+cls
+python main.py
